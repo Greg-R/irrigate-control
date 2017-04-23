dbb3330cf84b8672a8f506a5da0497eb8e46096c
Greg-R <soapy-smith@comcast.net>
2017-04-23 19:07:18 -0400

    Bug fixes to Proxy

--- software/node/ledActuator.js
+++ software/node/ledActuator.js
@@ -36,9 +36,9 @@ module.exports = class ledActuator extends EventEmitter {
     ledControl(ledgpio, command) {
         console.log('ledControl method was called!');
         const exec = require('child_process').exec;
-        console.log(`ledgpio = ${ledgpio} and this.ledGpioMap[ledgpio] = ${this.ledGpioMap[ledgpio]}`);
+        console.log(`ledgpio = ${ledgpio} and this.ledGpioMap[ledgpio] = ${this.ledGpioMap.get(ledgpio)}`);
         console.log(`echoing this command: echo ${command} > ${this.ledGpioMap[ledgpio]}`);
-        exec(`echo ${command} > ${this.ledGpioMap[ledgpio]}`, (error, stdout, stderr) => {
+        exec(`echo ${command} > ${this.ledGpioMap.get(ledgpio)}`, (error, stdout, stderr) => {
             if (error) {
                 console.error(`exec error: ${error}`);
                 return;
