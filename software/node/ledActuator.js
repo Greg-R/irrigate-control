9065002b2fe89e20a2cb6b56c7894bfc9143f5bc
Greg-R <soapy-smith@comcast.net>
2017-04-23 17:01:23 -0400

    Added ledActuator

---
+++ software/node/ledActuator.js
@@ -0,0 +1,45 @@
+/*jshint esversion: 6 */
+
+//  Led Control Actuator Object
+
+let EventEmitter = require('events').EventEmitter;
+const exec = require('child_process').exec;
+
+module.exports = class ledActuator extends EventEmitter {
+    constructor() {
+        this.ledHash = {
+            pumpmotor: "off",
+            zone1: "off",
+            zone2: "off"
+        };
+        this.ledHashProxy = new Proxy(this.ledHash, this.ledObserver);
+        //  Using pins 8.7 (gpio66), 8.8 (gpio67), and 8.10 (gpio68).
+        this.ledGpioMap = new Map([['pumpmotor', '/sys/class/gpio/gpio66/value'], 
+                                   ['zone1', '/sys/class/gpio/gpio67/value'], 
+                                   ['zone2', '/sys/class/gpio/gpio66/value']]);
+    }
+
+    ledObserver() {
+        return {
+            set: function (target, property, value, receiver) {
+                console.log(`Setting the LED value to ${value}.`);
+                this.ledControl(property, value);
+            }
+        };
+    }
+
+    //  This method does system calls on /sys to control the LEDs.
+    ledControl(ledgpio, command) {
+        const exec = require('child_process').exec;
+        exec(`echo ${command} > ${this.ledGpioMap[ledgpio]}`, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`exec error: ${error}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+            console.log(`stderr: ${stderr}`);
+        });
+    }
+
+
+};
--- software/node/websocketserver.js
+++ software/node/websocketserver.js
@@ -4,6 +4,9 @@
 
 //  Get the class:
 let WebSocketServer = require('ws').Server;
+let ledActuator = require('./ledActuator');
+
+let ledObject = new ledActuator();
 
 exports.listen = function (server) {
     var wss = new WebSocketServer({
@@ -16,6 +19,9 @@ exports.listen = function (server) {
         console.log(`The connection is open and request is from ${url}.`);
         ws.on('message', function (data, flags) { //#D subscribe to event.
             console.log(`Received data from client: ${data}.`);
+            let controlObject = JSON.parse(data);
+            //  Modify the ledHash object.  This will actuate the LEDs!
+            Object.assign(ledObject.ledHash, controlObject);
         });
     });
 
