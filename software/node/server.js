c4af0c106efc6a361a639aac397c5ed5a3a05d2b
Greg-R <soapy-smith@comcast.net>
2017-04-23 10:00:34 -0400

    Debugging server

--- software/node/server.js
+++ software/node/server.js
@@ -3,32 +3,32 @@
 //  Main server for irrigate-control
 
 let http = require('http');
-//let fs = require('fs');
+let fs = require('fs');
 let base = './../html';
- // let wsServer = require('./websocketserver');
+let wsServer = require('./websocketserver');
 
-let server = http.createServer(function(req, res) {
-    //let pathname = base + req.url;
-    //console.log(`The url from the request is ${pathname}.`);
-    
+let server = http.createServer(function (req, res) {
+    let pathname = base + req.url;
+    console.log(`The url from the request is ${pathname}.`);
+
     //  Respond to the request:
-//    res.setHeader('Content-type', 'text/html');
+    res.setHeader('Content-type', 'text/html');
     //  Create and pipe a readable stream:
-    //let file = fs.createReadStream(pathname);
-/*    file.on('open', function() {
+    let file = fs.createReadStream(pathname);
+    file.on('open', function () {
         // 200 status found, no errors.
         res.statusCode = 200;
-    //    file.pipe(res);
+        file.pipe(res);
     });
-    file.on('error', function(err) {
+    file.on('error', function (err) {
         res.writeHead(403);
         res.write('file missing, or permission problem');
         console.log(err);
-    });*/
-    
+    });
+
 }).listen(8089);
 
-// Websockets server
-//  wsServer.listen(server);
+//Websockets server
+wsServer.listen(server);
 
 console.log('Server is running at port 8089.');
