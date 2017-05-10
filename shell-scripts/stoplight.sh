63f2841c74066ee032007dfeb7d286d30a51b071
Greg-R <soapy-smith@comcast.net>
2017-05-10 12:21:44 -0400

    Changed start-up value of GPIOs to be high.

--- shell-scripts/stoplight.sh
+++ shell-scripts/stoplight.sh
@@ -1,4 +1,6 @@
+#  The solid-state relays are active low,
+#  so set the GPIOs to high upon start-up.
 
-config-pin P9.14 gpio high_pd
-config-pin P9.15 gpio high_pd
-config-pin P9.16 gpio high_pd
+config-pin P9.14 high_pd
+config-pin P9.15 high_pd
+config-pin P9.16 high_pd
