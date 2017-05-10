bf66f7bdc6edea2ba8803cff5b56ebaa78773754
Greg-R <soapy-smith@comcast.net>
2017-05-10 12:12:19 -0400

    Changed GPIOs to default high due to SSRs.

--- shell-scripts/stoplight.sh
+++ shell-scripts/stoplight.sh
@@ -1,4 +1,4 @@
 
-config-pin P9.14 gpio low_pd
-config-pin P9.15 gpio low_pd
-config-pin P9.16 gpio low_pd
+config-pin P9.14 gpio high_pd
+config-pin P9.15 gpio high_pd
+config-pin P9.16 gpio high_pd
