d94682f34e9aa2c54c8026c7a3f5ff7253a25ad7
Greg-R <soapy-smith@comcast.net>
2017-05-28 14:38:50 -0400

    Troubleshooting ws clients issues.

--- software/node/websocketserver.js
+++ software/node/websocketserver.js
@@ -1,46 +1,25 @@
 /*jshint esversion: 6 */
-
 //  This is the WebSockets server for the Irrigation control.
 
-//  Get the class:
+//  Get the WebSocket class:
 const WebSocketServer = require('ws').Server;
 let ledActuator = require('./ledActuator');
-
-//  Create an ledActuator Object.
-//let ledObject = new ledActuator();
 
 exports.listen = function (server) {
     const wss = new WebSocketServer({
         server: server
     });
-    
-    function heartbeat() {
-        this.isAlive = true;
-    }
-    
+
     console.info('WebSocket server started...');
     wss.on('connection', function (ws) {
-   //     ws.isAlive = true;
-   //     ws.on('pong', heartbeat);
-
+        //  Note the use of let for ledObject; the Object will be destroyed if the wss is destroyed.
         let ledObject = new ledActuator();
-        
+
         let url = ws.upgradeReq.url;
         console.info(url);
         console.log(`The connection is open and request is from ${url}.`);
-
-        //  Scan the connected sockets and write to the console.
-        //  The clients property is a set.
-        //        wss.clients.forEach(function each(client) {
-        //           console.log("Printing size of set of WebSocket clients.");
-        //      console.log(client);
-        //       });
-
         console.log(`The number of clients is ${wss.clients.size}`);
 
-        //console.log(`The WebSocket clients are ${wss.clients}.`);
-
-        //  Handle message sent from Web Browser controller page.
         ws.on('message', function (data, flags) {
             console.log(`Received data from client: ${data}.`);
             let controlObject = JSON.parse(data);
@@ -51,21 +30,13 @@ exports.listen = function (server) {
         //  Send messages to the web page indicating control status.
         ledObject.on('statusmessage', function (message) {
             console.log(`Status message received by websocketserver and is: ${message}`);
-
-            //  Ping sockets and kill the dead ones.
+            //  Send status message if the WebSocket is ready.  Terminate defective WebSockets.
             if (ws.readyState === 1) {
                 console.log("WebSocket is ready and sending status to Web Page.");
                 ws.send(message);
-                console.log("Sending status message to web page.");
             } else {
-                (console.log(`Websocket was not ready and readyState is ${ws.readyState}.`));
-                // Kill unready the WebSocket.
                 console.log("Killing a defective websocket.");
                 ws.terminate();
-                //  Also, remove the Event Listener.
-                ledObject.removeListener('statusmessage', () => {
-                    console.log("statusmessage event removed from ledObject.");
-                });
             }
         });
     });
@@ -76,13 +47,5 @@ exports.listen = function (server) {
             console.log("Closing WebSocketServer.");
         });
     });
-
-//    const interval = setInterval(function ping() {
-//        wss.clients.forEach(function each(ws) {
-//            if (ws.isAlive === false) return ws.terminate();
-//            ws.isAlive = false;
- //           ws.ping('', false, true);
- //       });
-//    }, 1000);
 
 };
