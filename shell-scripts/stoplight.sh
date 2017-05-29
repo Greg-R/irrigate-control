b70d3a67d7a9b16069ae352aad2773d634402e15
Greg-R <soapy-smith@comcast.net>
2017-05-29 13:10:53 -0400

    Started updating documentation.

--- shell-scripts/stoplight.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#  Configure pins to GPIOs and set low.
-#  This filled is called in .bashrc in /home/debian.
-
-config-pin P9.14 low_pd
-config-pin P9.15 low_pd
-config-pin P9.16 low_pd
